d096708271061d529803bf399dad499f145b8c5b
Mohamed-Elguindy <mohamedwelguindy@gmail.com>
2025-11-22 21:25:25 +0200

    Setup

---
+++ src/main/java/com/elguindy/taskmanager_12project/TaskManager12ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.elguindy.taskmanager_12project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager12ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManager12ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elguindy/taskmanager_12project/TaskManager12ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elguindy.taskmanager_12project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManager12ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
