08bc7db3176aff696943a3b31ebe80753855de30
Mohamed-Elguindy <mohamedwelguindy@gmail.com>
2025-11-25 13:23:45 +0200

    for trying front

--- src/main/java/com/elguindy/taskmanager_12project/controller/TaskController.java
+++ src/main/java/com/elguindy/taskmanager_12project/controller/TaskController.java
@@ -1,4 +1,12 @@
 package com.elguindy.taskmanager_12project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class TaskController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "index";
+    }
 }
--- src/main/java/com/elguindy/taskmanager_12project/entity/TaskEntity.java
+++ src/main/java/com/elguindy/taskmanager_12project/entity/TaskEntity.java
@@ -1,4 +1,15 @@
 package com.elguindy.taskmanager_12project.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class TaskEntity {
+    @Id
+    private Long id;
+
+    private String title;
+
 }
